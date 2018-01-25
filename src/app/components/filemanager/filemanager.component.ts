15cec7d08a909757ecc76b1c0521d48905e1f30c
RYTM <mail@steffen-knuedel.de>
2018-01-25 23:27:23 +0100

    javascript for filemanager, needs refinement..

--- src/app/components/filemanager/filemanager.component.ts
+++ src/app/components/filemanager/filemanager.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+const userHome = require('user-home')
 const fs = require("fs")
 
 @Component({
@@ -7,18 +8,63 @@ const fs = require("fs")
   styleUrls: ['./filemanager.component.scss']
 })
 export class FilemanagerComponent implements OnInit {
-
+  initialFiles = []
   constructor() { }
 
   getFolderStructure(folder) {
-    fs.readdir(folder, (err, file) => {
-      console.log(file)
-      this.getFolderStructure(file)
+    fs.readdir(folder, (err, files) => {
+      if(err) throw err
+      files.map(file => {
+        fs.stat(folder + file, (err, stats) => {
+          if(err) throw err
+          if(stats.isDirectory()) {
+            this.initialFiles.push({name: file, directory: true, path: folder + file + "/"})
+          } else {
+            this.initialFiles.push({name: file, directory: false, path: folder + file})
+          }
+        })
+      })
     })
   }
 
+  getSubDirectories(folder) {
+    console.log(event.path[1])
+    const target = event.path[1]
+    const subList = document.createElement("ul")
+    subList.classList.add("rs-filemanager-list-block")
+
+    fs.readdir(folder, (err, files) => {
+      if(err) throw err
+
+      files.map(file => {
+        const listElement = document.createElement("li")
+        listElement.classList.add("rs-filemanager-list-element")
+
+        fs.stat(folder + file, (err, stats) => {
+          if(err) throw err
+
+          if(stats.isDirectory()) {
+            listElement.innerHTML = `
+            <span class="rs-list-element__folder" (click)="getSubDirectories(${folder + file}/)">
+              ${file}
+            </span>`
+            subList.appendChild(listElement)
+          } else {
+            listElement.innerHTML = `
+            <span class="rs-list-element__file">
+              ${file}
+            </span>`
+            subList.appendChild(listElement)
+          }
+        })
+      })
+
+    })
+    target.appendChild(subList)
+  }
+
   ngOnInit() {
-    this.getFolderStructure(".")
+    this.getFolderStructure(`${userHome}/Documents/`)
   }
 
 }
