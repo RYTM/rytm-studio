cc9919dd19ee8fc11a9717814eef84afbc5c9872
RYTM <mail@steffen-knuedel.de>
2018-01-28 12:29:21 +0100

    sidebar switch tab function

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,11 +9,17 @@ export class SidebarComponent implements OnInit {
 
   constructor() { }
 
-  switchSidebarTabs() {
+  switchSidebarTabs(content) {
     const tabs: Element[] = Array.from(document.querySelectorAll('.rs-tab-block__element'))
     const tab: any = event.target
     tabs.map(t => t.classList.remove("rs-tab-block__element--active"))
     tab.classList.add("rs-tab-block__element--active")
+
+    const contentBlock = document.querySelector(".rs-sidebar-content-block")
+    const contentBlockWidth = contentBlock.clientWidth
+
+    contentBlock.style.marginLeft = `-${contentBlockWidth * content}px`
+
   }
 
   ngOnInit() {
